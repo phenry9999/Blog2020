8aa2b2d669b3a66718e4f17cdf9a7afedffe775d
PHenry <peter.henry@pchenry.com>
2020-09-11 00:29:48 -0400

    Before refactoring

---
+++ Blog2020/SalaryCalculator/SalaryCalculator.Tests/Calculator/SalaryCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	public class SalaryCalculator
+	{
+		private const int hoursInYear = 52 * (5 * 8);       //2080 = weeks in a year * days a week * hours a day
+
+		/// <summary>
+		/// To get annual, multiply hourly by 2080
+		/// </summary>
+		/// <param name="hourlyWage"></param>
+		/// <returns></returns>
+		public decimal GetAnnualSalary(decimal hourlyWage)
+		{
+			decimal annualSalary = hoursInYear * hourlyWage;
+			return annualSalary;
+		}
+
+		/// <summary>
+		/// To get hourly, divide annual salary by 2080
+		/// </summary>
+		/// <param name="annualSalary"></param>
+		/// <returns></returns>
+		public decimal GetHourlyWage(decimal annualSalary) => annualSalary / hoursInYear;
+	}
+}
---
+++ Blog2020/SalaryCalculator/SalaryCalculator.Tests/SalaryCalculatorTests/CalculatorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Security.Policy;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SalaryCalculatorTests
+{
+	[TestClass]
+	public class CalculatorTests
+	{
+		static SalaryCalculator sc = null;
+		decimal decimalHourlyWageValue = 48.08M;
+		decimal decimalAnnualSalaryValue = 100006.40M;
+
+		[ClassInitialize]
+		public static void TestFixtureSetup(TestContext testContext)
+		{
+			sc = new SalaryCalculator();
+		}
+
+		[ClassCleanup]
+		public static void TestFixtureCleanup()
+		{
+			sc = null;
+		}
+
+
+		[TestMethod]
+		public void AnnualSalaryCalculation_GivenRoundConsultingRate_ThenRoundSalaryValue()
+		{
+			//arrange
+
+			//act
+			decimal annualSalary = sc.GetAnnualSalary(50);
+
+			//assert
+			Assert.AreEqual(104000, annualSalary, "You didn't get the right value for the annual salary calculation");
+		}
+
+		[TestMethod]
+		public void AnnualSalaryCalculation_GivenDecimalValuedConsultingRate_ThenDecimalSalaryValue()
+		{
+			//arrange
+
+			//act
+			decimal annualSalary = sc.GetAnnualSalary(decimalHourlyWageValue);
+
+			//assert
+			Assert.AreEqual(decimalAnnualSalaryValue, annualSalary, "You didn't get the right value for the annual salary calculation");
+		}
+
+		[TestMethod]
+		public void HourlySalaryCalculation_GivenRoundAnnualRate_ThenRoundHourlyValue()
+		{
+			//arr
+
+			//act
+			decimal hourlyWage = sc.GetHourlyWage(104000);
+
+			//ass
+			Assert.AreEqual(50, hourlyWage);
+		}
+
+		[TestMethod]
+		public void HourlySalaryCalculation_GivenDecimalAnnualRate_ThenDecimalHourlyValue()
+		{
+			//arr
+
+			//act
+			decimal hourlyWage = sc.GetHourlyWage(decimalAnnualSalaryValue);
+
+			//ass
+			Assert.AreEqual(decimalHourlyWageValue, hourlyWage);
+		}
+	}
+}
